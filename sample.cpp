b2f2acb9af2feb59e87ec639b9ed8a7bf1eb9006
vidy1 <vidya.marathe@cumminscollege.in>
2016-08-11 10:57:49 +0530

    Update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,4 +1,6 @@
 this is a sample text 
 learning how to use git hub
 this is a bit weird
-now editing the file. add new line
+now editing the file. new line was added
+editing from second branch
+deleting a random line
